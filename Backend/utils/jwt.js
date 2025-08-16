965130088746152f222773045e63e4b721e60633
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:30:24 +0530

    refactor jwt.js: remove commented-out code for attachSingleCookieToResponse function

--- Backend/utils/jwt.js
+++ Backend/utils/jwt.js
@@ -28,18 +28,6 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
     expires: new Date(Date.now() + longerExp),
   });
 };
-// const attachSingleCookieToResponse = ({ res, user }) => {
-//   const token = createJWT({ payload: user });
-
-//   const oneDay = 1000 * 60 * 60 * 24;
-
-//   res.cookie('token', token, {
-//     httpOnly: true,
-//     expires: new Date(Date.now() + oneDay),
-//     secure: process.env.NODE_ENV === 'production',
-//     signed: true,
-//   });
-// };
 
 module.exports = {
   createJWT,
