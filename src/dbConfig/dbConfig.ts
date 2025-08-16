d3fe56367317a1e22b69087d438505dba74152f0
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:35:08 +0530

    refactor(dbConfig.ts): improve formatting and enhance error logging for MongoDB connection

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -5,17 +5,16 @@ export async function connect() {
         mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
 
-        connection.on('connected', ()=>{
+        connection.on('connected', () => {
             console.log('MongoDB connected successfully');
         })
 
-        connection.on('error', (err)=>{
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+        connection.on('error', (err) => {
+            console.log(err);
             process.exit();
         })
-        
+
     } catch (error) {
-        console.log('Something goes wrong!');
         console.log(error);
     }
 }
