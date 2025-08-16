a039b2e501676687d3eab99c280c29f6dfa8bf64
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:30:41 +0530

    refactor nodemailerConfig.js: improve code formatting for consistency

--- Backend/utils/nodemailerConfig.js
+++ Backend/utils/nodemailerConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: 'c935536c7138ad',
-      pass: '7ddd1ea278dd33',
-    },
-  };
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'c935536c7138ad',
+    pass: '7ddd1ea278dd33',
+  },
+};
