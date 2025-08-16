7de2748e878149a36e69c7e56569798f200591e2
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:39:12 +0530

    refactor(mailer.ts): improve formatting

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -3,27 +3,26 @@ import User from "@/models/userModel";
 import bcryptjs from 'bcryptjs';
 
 
-export const sendEmail = async({email, emailType, userId}:any) => {
+export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
-        // create a hased token
         const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
         if (emailType === "VERIFY") {
-            await User.findByIdAndUpdate(userId, 
-                {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
-        } else if (emailType === "RESET"){
-            await User.findByIdAndUpdate(userId, 
-                {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
+            await User.findByIdAndUpdate(userId,
+                { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 })
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId,
+                { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 })
         }
 
-        var transport = nodemailer.createTransport({
+        const transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
-              user: "336e6799376bac",
-              pass: "f8320e0ed94e2a"
+                user: "336e6799376bac",
+                pass: "f8320e0ed94e2a"
             }
-          });
+        });
 
 
         const mailOptions = {
@@ -35,11 +34,10 @@ export const sendEmail = async({email, emailType, userId}:any) => {
             </p>`
         }
 
-        const mailresponse = await transport.sendMail
-        (mailOptions);
+        const mailresponse = await transport.sendMail(mailOptions);
         return mailresponse;
 
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.message);
     }
 }
