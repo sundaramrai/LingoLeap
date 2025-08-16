af55d44f84819955b71dff355d3bbca02a017d1c
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-17 00:41:15 +0530

    refactor(mailer.ts): update token generation for email verification and reset to use random bytes

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,18 +1,20 @@
 import nodemailer from 'nodemailer';
 import User from "@/models/userModel";
 import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
 
 
 export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
-        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+        const rawToken = crypto.randomBytes(32).toString('hex');
+        const hashedToken = await bcryptjs.hash(rawToken, 10);
 
         if (emailType === "VERIFY") {
             await User.findByIdAndUpdate(userId,
-                { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 })
+                { verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000 });
         } else if (emailType === "RESET") {
             await User.findByIdAndUpdate(userId,
-                { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 })
+                { forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000 });
         }
 
         const transport = nodemailer.createTransport({
@@ -24,13 +26,19 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
             }
         });
 
+        const backendDomain =
+            process.env.NODE_ENV === "production"
+                ? process.env.NEXT_PUBLIC_API_URL
+                : process.env.DOMAIN;
+        const urlPath = emailType === "VERIFY" ? "verifyemail" : "resetpassword";
+        const url = `${backendDomain}/${urlPath}?token=${rawToken}&id=${userId}`;
 
         const mailOptions = {
             from: 'pratham@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            html: `<p>Click <a href="${url}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${url}
             </p>`
         }
 
