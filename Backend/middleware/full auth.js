7eed9f2e539a7c9dcbb658465b8cb3470636089d
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:27:56 +0530

    refactor auth.js: remove unnecessary comments and improve error handling in authenticateUser function

--- Backend/middleware/full auth.js
+++ Backend/middleware/full auth.js
@@ -3,12 +3,10 @@ const { isTokenValid } = require('../utils/jwt');
 
 const authenticateUser = async (req, res, next) => {
   let token;
-  // check header
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader.startsWith('Bearer')) {
     token = authHeader.split(' ')[1];
   }
-  // check cookies
   else if (req.cookies.token) {
     token = req.cookies.token;
   }
@@ -18,8 +16,6 @@ const authenticateUser = async (req, res, next) => {
   }
   try {
     const payload = isTokenValid(token);
-
-    // Attach the user and his permissions to the req object
     req.user = {
       userId: payload.user.userId,
       role: payload.user.role,
@@ -27,7 +23,7 @@ const authenticateUser = async (req, res, next) => {
 
     next();
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication invalid');
+    throw new CustomError.UnauthenticatedError(error);
   }
 };
 
