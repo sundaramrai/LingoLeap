53e421f037d32c7f2848c904c5da32965c736a93
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:31:44 +0530

    refactor sendEmail.js: remove unused test account creation

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -2,12 +2,10 @@ const nodemailer = require('nodemailer');
 const nodemailerConfig = require('./nodemailerConfig');
 
 const sendEmail = async ({ to, subject, html }) => {
-  let testAccount = await nodemailer.createTestAccount();
-
   const transporter = nodemailer.createTransport(nodemailerConfig);
 
   return transporter.sendMail({
-    from: '"Coding Addict" <codingaddict@gmail.com>', // sender address
+    from: '"Coding Addict" <codingaddict@gmail.com>',
     to,
     subject,
     html,
