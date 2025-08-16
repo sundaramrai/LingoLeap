0fc91432c07d81cfb1e56d80b9adeacbefc4ae70
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:43:37 +0530

    refactor(page.jsx): remove unused import and replace SVG logo with image

--- src/app/user/startquiz/page.jsx
+++ src/app/user/startquiz/page.jsx
@@ -1,6 +1,4 @@
 'use client'
-
-import Link from 'next/link';
 import Navbar from '@/app/components/Navbar';
 import Footer from '@/app/components/Footer';
 
@@ -15,10 +13,7 @@ const quiz = () => {
           <div className="flex flex-col items-center">
             {/* <!-- logo - start --> */}
             <a href="/" className="mb-8 inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
-              <svg width="95" height="94" viewBox="0 0 95 94" className="h-auto w-6 text-red-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-              </svg>
-
+              <img src='/lingoleap.png' style={{ width: '30px', height: '30px' }} alt="LingoLeap Logo" />
               LingoLeap
             </a>
             {/* <!-- logo - end --> */}
