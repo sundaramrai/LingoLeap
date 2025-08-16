013da24236d3198d74e9a5296c1cc5df1a0d6d3e
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:25:28 +0530

    refactor server.js: clean up comments and formatting

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const connectDB = require('./db/connect');
 
-//routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -20,12 +19,11 @@ app.use(cookieParser(process.env.JWT_SECRET));
 
 
 app.get('/api/v1', (req, res) => {
-    res.send('<h1>SIH</h1>');
+  res.send('<h1>SIH</h1>');
 });
 
-//routes
 app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/user',userRouter);
+app.use('/api/v1/user', userRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
