97e0bcc1a41c997c7afea761d8ae667bdb35c5dd
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-16 20:28:12 +0530

    refactor user.js: improve code formatting and consistency

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,49 +3,49 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:[true,'Please provide a name'],
-        maxlength:50,
-        minlength:3
-    },
-    email:{
-        type:String,
-        required: [true, 'Please provide email'],
-        match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            'Please provide a valid email',
-          ],
-        unique: true
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minlength: 6
-    },
-    // supervisor_email:{
-    //     type:String,
-    // }
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-      experience: {
-        type: Number,
-        default: 0,
-      },
-      verificationToken: String,
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
-      verified: Date,
-      passwordToken: {
-        type: String,
-      },
-      passwordTokenExpirationDate: {
-        type: Date,
-      },
+  username: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    maxlength: 50,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email',
+    ],
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6
+  },
+  // supervisor_email:{
+  //     type:String,
+  // }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  experience: {
+    type: Number,
+    default: 0,
+  },
+  verificationToken: String,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
+  passwordToken: {
+    type: String,
+  },
+  passwordTokenExpirationDate: {
+    type: Date,
+  },
 });
 
 UserSchema.pre('save', async function () {
